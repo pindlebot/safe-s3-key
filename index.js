1975ce736158aa16df67e5d24d02a4e151a2135f
Ben <menubar@protonmail.com>
2018-10-05 06:26:13 -0400

    new

---
+++ index.js
@@ -0,0 +1,16 @@
+const crypto = require('crypto')
+
+module.exports = (name) => {
+  if (typeof name !== 'string') {
+    if (name.hasOwnProperty('toString')) {
+      name = name.toString()
+    } else {
+      throw new Error('argument must be a string')
+    }
+  }
+  if (name.startsWith('/')) {
+    name = name.slice(1, name.length)
+  }
+  let filename = decodeURIComponent(name).replace(/[^A-Za-z0-9!-_.*'()]/g, '')
+  return filename || crypto.randomBytes(10).toString('hex')
+}
